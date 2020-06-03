1c265613aaf359216d2cd62e6289c01b0f1ab60b
aquinojosue <aquinojosue3@gmail.com>
2020-06-03 14:58:30 -0600

    Agregado nombre de la columna para que coincida con los modelos hechos.

--- app/src/main/java/com/grupo13/inventario/modelo/Idiomas.java
+++ app/src/main/java/com/grupo13/inventario/modelo/Idiomas.java
@@ -1,12 +1,20 @@
 package com.grupo13.inventario.modelo;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Idiomas {
-    @PrimaryKey(autoGenerate = true) int idIdioma;
+    //En casos que el nombre del atributo no coincida con el de la clase
+    //Se puede usar @ColumnInfo
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "idioma_id")
+    int idIdioma;
+
+    @ColumnInfo(name = "idioma_nombre")
     String nombreIdioma;
 
     @Ignore
