32e286c83508eb99277c08b3823146fc2dfdbf0b
aquinojosue <aquinojosue3@gmail.com>
2020-06-03 14:28:42 -0600

    Creación del paquete DAO e implementación del DAO para Idiomas

---
+++ app/src/main/java/com/grupo13/inventario/dao/IdiomasDao.java
@@ -0,0 +1,26 @@
+package com.grupo13.inventario.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.grupo13.inventario.modelo.Idiomas;
+
+import java.util.List;
+
+@Dao
+public interface IdiomasDao {
+    @Query("SELECT * FROM Idiomas")
+    List<Idiomas> obtenerIdiomas();
+
+    @Insert
+    void insertarIdioma(Idiomas idioma);
+
+    @Update
+    void actualizarIdioma(Idiomas idioma);
+
+    @Delete
+    void eliminarIdioma(Idiomas idioma);
+}
