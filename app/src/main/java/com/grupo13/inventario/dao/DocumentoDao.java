cf9479c9ad1dc0ea0b1f6c1ecb81879805fb5706
aquinojosue <aquinojosue3@gmail.com>
2020-06-03 16:12:33 -0600

    Agregar Insert al DAO de Documento

--- app/src/main/java/com/grupo13/inventario/dao/DocumentoDao.java
+++ app/src/main/java/com/grupo13/inventario/dao/DocumentoDao.java
@@ -2,6 +2,7 @@ package com.grupo13.inventario.dao;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
@@ -15,6 +16,9 @@ public interface DocumentoDao {
 
     @Query("SELECT * FROM Documento WHERE escrito_id = :escrito_id")
     Documento consultarDocumento(int escrito_id);
+
+    @Insert
+    void insertarDocumento(Documento documento);
 
     @Update
     void actualizarDocumento(Documento documento);
