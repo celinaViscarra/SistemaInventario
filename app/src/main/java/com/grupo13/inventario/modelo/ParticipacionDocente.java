181e252767c5413c355c79ea9a542aae1edfea65
aquinojosue <aquinojosue3@gmail.com>
2020-06-04 14:12:04 -0600

    Implementacion de Relaciones de ParticipacionDocente con TipoParticipacion, Docente y Documento

--- app/src/main/java/com/grupo13/inventario/modelo/ParticipacionDocente.java
+++ app/src/main/java/com/grupo13/inventario/modelo/ParticipacionDocente.java
@@ -2,9 +2,34 @@ package com.grupo13.inventario.modelo;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Ignore;
 
-@Entity(primaryKeys = {"escrito_id","docentes_id"})
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(
+        primaryKeys = {"escrito_id","docentes_id"},
+        foreignKeys = {
+                @ForeignKey(
+                        entity = TipoParticipacion.class,
+                        parentColumns = "participacion_id",
+                        childColumns = "participacion_id",
+                        onDelete = CASCADE
+                ),
+                @ForeignKey(
+                        entity = Docente.class,
+                        parentColumns = "docentes_id",
+                        childColumns = "docentes_id",
+                        onDelete = CASCADE
+                ),
+                @ForeignKey(
+                        entity = Documento.class,
+                        parentColumns = "escrito_id",
+                        childColumns = "escrito_id",
+                        onDelete = CASCADE
+                )
+        }
+)
 public class ParticipacionDocente {
     @ColumnInfo(name = "escrito_id")
     public int idDocentes;
