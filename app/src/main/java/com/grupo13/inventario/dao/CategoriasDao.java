071569b06c0a2883960a9355e56023c8358eb368
aquinojosue <aquinojosue3@gmail.com>
2020-06-04 08:22:18 -0600

    Fix Clase Consulta Relacion Categoria con Tipo Producto

--- app/src/main/java/com/grupo13/inventario/dao/CategoriasDao.java
+++ app/src/main/java/com/grupo13/inventario/dao/CategoriasDao.java
@@ -7,6 +7,7 @@ import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
+import com.grupo13.inventario.modelo.CategoriaConTipoProducto;
 import com.grupo13.inventario.modelo.Categorias;
 import com.grupo13.inventario.modelo.TipoProducto;
 
@@ -22,11 +23,11 @@ public interface CategoriasDao {
 
     @Transaction
     @Query("SELECT * FROM Categorias")
-    List<TipoProducto> obtenerTipoProductosPorCategorias();
+    List<CategoriaConTipoProducto> obtenerTipoProductosPorCategorias();
 
     @Transaction
     @Query("SELECT * FROM Categorias WHERE categoria_id = :categoria_id")
-    List<TipoProducto> consultarTipoProductosPorCategoria(int categoria_id);
+    List<CategoriaConTipoProducto> consultarTipoProductosPorCategoria(int categoria_id);
 
     @Insert
     void insertarCategoria(Categorias categorias);
