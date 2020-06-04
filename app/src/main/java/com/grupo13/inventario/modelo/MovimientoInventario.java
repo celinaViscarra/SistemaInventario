f43f0ee4cc1a50b3ad281bd1ca9ff672c8f016e4
aquinojosue <aquinojosue3@gmail.com>
2020-06-04 16:45:37 -0600

    Implementacion de Relaciones de MovimientoInventario con TipoMovimiento y Docente

--- app/src/main/java/com/grupo13/inventario/modelo/MovimientoInventario.java
+++ app/src/main/java/com/grupo13/inventario/modelo/MovimientoInventario.java
@@ -2,12 +2,30 @@ package com.grupo13.inventario.modelo;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.sql.Date;
 
-@Entity
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(
+        foreignKeys = {
+                @ForeignKey(
+                        entity = TipoMovimiento.class,
+                        parentColumns = "tipo_movimiento_id",
+                        childColumns = "tipo_movimiento_id",
+                        onDelete = CASCADE
+                ),
+                @ForeignKey(
+                        entity = Docente.class,
+                        parentColumns = "docentes_id",
+                        childColumns = "docented_id",
+                        onDelete = CASCADE
+                )
+        }
+)
 public class MovimientoInventario {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "prestamo_id")
@@ -28,6 +46,9 @@ public class MovimientoInventario {
     String descripcion;
 
     int docentes_id;
+
+    int tipo_movimiento_id;
+    int equipo_id;
 
     @Ignore
     public MovimientoInventario(){
