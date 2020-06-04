c8ab190293179905e9d8c1877cc749a69b9b09fa
Celina <xelaju_celina@yahoo.com>
2020-06-03 22:43:36 -0600

    Actualización de TipoMovimiento y Sustituciones.

--- app/src/main/java/com/grupo13/inventario/modelo/TipoMovimiento.java
+++ app/src/main/java/com/grupo13/inventario/modelo/TipoMovimiento.java
@@ -1,9 +1,20 @@
 package com.grupo13.inventario.modelo;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class TipoMovimiento {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "tipo_movimiento_id")
     int idTipoMovimiento;
+
+    @ColumnInfo(name = "tipo_movimiento_nombre")
     String nombreTipoMoviento;
 
+    @Ignore
     public TipoMovimiento(){}
 
     public TipoMovimiento(int id, String nom){
