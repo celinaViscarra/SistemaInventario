f54b68f354f2adc1cf5be29c923ef6954eb0a2fb
aquinojosue <aquinojosue3@gmail.com>
2020-06-04 13:28:30 -0600

    Implementacion de Relaciones de DetalleAutor con Autor y Documento

--- app/src/main/java/com/grupo13/inventario/modelo/DetalleAutor.java
+++ app/src/main/java/com/grupo13/inventario/modelo/DetalleAutor.java
@@ -1,9 +1,21 @@
 package com.grupo13.inventario.modelo;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.Ignore;
 
-@Entity(primaryKeys = {"escrito_id","idAutor"})
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(primaryKeys = {"escrito_id","idAutor"}, foreignKeys = {
+        @ForeignKey(entity = Documento.class,
+                    parentColumns = "escrito_id",
+                    childColumns = "escrito_id",
+                    onDelete = CASCADE),
+        @ForeignKey(entity = Autor.class,
+                    parentColumns = "idAutor",
+                    childColumns = "idAutor",
+                    onDelete = CASCADE)
+})
 public class DetalleAutor {
     public int idAutor;
     public int escrito_id;
