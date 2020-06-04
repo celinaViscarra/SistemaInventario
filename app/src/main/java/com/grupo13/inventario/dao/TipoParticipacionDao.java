f4f1030bb329c5d3156e801eec1f0f4428ee6efe
aquinojosue <aquinojosue3@gmail.com>
2020-06-04 08:01:03 -0600

    Implementacion de Entidad y DAO TipoParticipacion

--- app/src/main/java/com/grupo13/inventario/dao/TipoParticipacionDao.java
+++ app/src/main/java/com/grupo13/inventario/dao/TipoParticipacionDao.java
@@ -1,5 +1,6 @@
 package com.grupo13.inventario.dao;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -8,6 +9,7 @@ import androidx.room.Update;
 import com.grupo13.inventario.modelo.TipoParticipacion;
 import java.util.List;
 
+@Dao
 public interface TipoParticipacionDao {
     @Query("SELECT * FROM TipoParticipacion")
     List<TipoParticipacion> obtenerTipoParticipaciones();
