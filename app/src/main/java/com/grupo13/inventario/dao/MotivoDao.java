ff2e9af7b5728b107a1452b3d1e156150300e821
Celina <xelaju_celina@yahoo.com>
2020-06-04 19:27:26 -0600

    Actualizacion dao.

---
+++ app/src/main/java/com/grupo13/inventario/dao/MotivoDao.java
@@ -0,0 +1,27 @@
+package com.grupo13.inventario.dao;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.grupo13.inventario.modelo.Motivo;
+
+import java.util.List;
+
+public interface MotivoDao {
+    @Query("SELECT * FROM Motivo")
+    List<Motivo> obtenerMotivos();
+
+    @Query("SELECT * FROM Motivo WHERE motivo_id=:idMotivo")
+    Motivo consultarMotivo(int idMotivo);
+
+    @Insert
+    void insertarMotivo(Motivo motivo);
+
+    @Update
+    void actualizarMotivo(Motivo motivo);
+
+    @Delete
+    void eliminarMotivo(Motivo motivo);
+}
