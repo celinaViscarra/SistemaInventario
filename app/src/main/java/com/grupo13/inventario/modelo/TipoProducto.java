a9e5e03f548b78bea603be9f82143338debad536
aquinojosue <aquinojosue3@gmail.com>
2020-06-05 13:40:41 -0600

    Varios fixes por problemas con Room.

--- app/src/main/java/com/grupo13/inventario/modelo/TipoProducto.java
+++ app/src/main/java/com/grupo13/inventario/modelo/TipoProducto.java
@@ -20,37 +20,22 @@ import static androidx.room.ForeignKey.CASCADE;
 public class TipoProducto {
     @NonNull
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name="tipo_producto_id")
-    int idTipoProducto;
+    @ColumnInfo(name = "tipo_producto_id")
+    public int idTipoProducto;
 
     @NonNull
     @ColumnInfo(name = "nombre_tipo_producto")
-    String nomTipoProducto;
+    public String nomTipoProducto;
 
-    int categoria_id;
+    public int categoria_id;
 
     @Ignore
-    public TipoProducto(){}
+    public TipoProducto() {
+    }
 
     public TipoProducto(int idTipoProducto, @NonNull String nomTipoProducto, int categoria_id) {
         this.idTipoProducto = idTipoProducto;
         this.nomTipoProducto = nomTipoProducto;
         this.categoria_id = categoria_id;
-    }
-
-    public int getIdTipoProducto() {
-        return idTipoProducto;
-    }
-
-    public void setIdTipoProducto(int idTipoProducto) {
-        this.idTipoProducto = idTipoProducto;
-    }
-
-    public String getNomTipoProducto() {
-        return nomTipoProducto;
-    }
-
-    public void setNomTipoProducto(String nomTipoProducto) {
-        this.nomTipoProducto = nomTipoProducto;
     }
 }
