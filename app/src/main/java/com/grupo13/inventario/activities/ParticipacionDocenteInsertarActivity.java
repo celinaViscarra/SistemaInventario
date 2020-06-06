e1e5484494d2da23224de8e0f59ab18e161c5eef
aquinojosue <aquinojosue3@gmail.com>
2020-06-06 08:47:31 -0600

    Implementacion de Insertar ParticipacionDocente

--- app/src/main/java/com/grupo13/inventario/activities/ParticipacionDocenteInsertarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/ParticipacionDocenteInsertarActivity.java
@@ -2,15 +2,112 @@ package com.grupo13.inventario.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
+import com.grupo13.inventario.modelo.Docente;
+import com.grupo13.inventario.modelo.Documento;
+import com.grupo13.inventario.modelo.ParticipacionDocente;
+import com.grupo13.inventario.modelo.TipoParticipacion;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ParticipacionDocenteInsertarActivity extends AppCompatActivity {
+    ControlBD helper;
+    @BindView(R.id.edtEscritoID)
+    Spinner edtEscritoID;
+    @BindView(R.id.edtDocenteID)
+    Spinner edtDocenteID;
+    @BindView(R.id.edtTipoParticipacionID)
+    Spinner edtTipoParticipacionID;
 
+    List<Docente> docentes;
+    List<Documento> documentos;
+    List<TipoParticipacion> tipoParticipaciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participacion_docente_insertar);
+        ButterKnife.bind(this);
+        helper = ControlBD.getInstance(this);
+        llenarSpinners();
+    }
+    public void insertarParticipacionDocente(View v){
+        ParticipacionDocente participacionDocente = new ParticipacionDocente();
+        //Para traerse los datos de los spinners
+        //De esta forma, cualquier entrada de datos es valida, por lo cual ya no se necesita el
+        //catch de NumberFormatException.
+        participacionDocente.idDocentes = docentes.get(edtDocenteID.getSelectedItemPosition()).idDocente;
+        participacionDocente.idEscritos = documentos.get(edtEscritoID.getSelectedItemPosition()).idEscrito;
+        participacionDocente.idParticipacion = tipoParticipaciones.get(edtTipoParticipacionID.getSelectedItemPosition()).idParticipacion;
+        String mensaje = "";
+        try{
+            long posicion = helper.participacionDocenteDao().insertarParticipacionDocente(participacionDocente);
+            if(posicion == 0 || posicion == -1){
+                mensaje = "Error al tratar de registrar la ParticipacionDocente";
+            } else{
+                mensaje = String.format("Registrado correctamente en la posicion: %d", posicion);
+            }
+        }
+        catch (SQLiteConstraintException e){
+            mensaje = "Error al tratar de registrar la ParticipacionDocente";
+        }
+        finally {
+            Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void llenarSpinners(){
+        //Primer paso, traerme la lista de docentes, documentos y tipoParticipaciones
+        docentes = helper.docenteDao().obtenerDocentes();
+        documentos = helper.documentoDao().obtenerDocumentos();
+        tipoParticipaciones = helper.tipoParticipacionDao().obtenerTipoParticipaciones();
+
+        //Segundo paso, hacer los arraylist que ocupare en los spinner
+        ArrayList<String> nombresDocentes = new ArrayList<>();
+        ArrayList<String> nombresDocumentos = new ArrayList<>();
+        ArrayList<String> nombresTipoParticiones = new ArrayList<>();
+
+        for(Documento pivote: documentos){
+            nombresDocumentos.add(pivote.titulo);
+        }
+        for(Docente pivote: docentes){
+            nombresDocentes.add(pivote.nomDocente);
+        }
+        for(TipoParticipacion pivote: tipoParticipaciones){
+            nombresTipoParticiones.add(pivote.nomParticipacion);
+        }
+
+        //Tercer paso, hacer los arrayadapters
+        ArrayAdapter docenteArrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                nombresDocentes);
+        ArrayAdapter documentoArrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                nombresDocumentos);
+        ArrayAdapter tipoParticipacionArrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                nombresTipoParticiones);
+
+        //Cuarto paso, poner los array adapters en los spinners correspondientes
+        edtEscritoID.setAdapter(documentoArrayAdapter);
+        edtDocenteID.setAdapter(docenteArrayAdapter);
+        edtTipoParticipacionID.setAdapter(tipoParticipacionArrayAdapter);
     }
 }
