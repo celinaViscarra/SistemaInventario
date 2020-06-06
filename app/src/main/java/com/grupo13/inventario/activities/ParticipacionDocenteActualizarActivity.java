9849241231688ba73e5e9b35f9ed4a27bb4e6760
aquinojosue <aquinojosue3@gmail.com>
2020-06-06 08:03:34 -0600

    Implementacion de Activities de ParticipacionDocente

---
+++ app/src/main/java/com/grupo13/inventario/activities/ParticipacionDocenteActualizarActivity.java
@@ -0,0 +1,16 @@
+package com.grupo13.inventario.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grupo13.inventario.R;
+
+public class ParticipacionDocenteActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_participacion_docente_actualizar);
+    }
+}
