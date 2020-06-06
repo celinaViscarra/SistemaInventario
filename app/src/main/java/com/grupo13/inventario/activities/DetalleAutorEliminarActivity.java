9cbd2a51815477d5c9186a8369c1f9b99cdc93aa
aquinojosue <aquinojosue3@gmail.com>
2020-06-05 23:13:34 -0600

    Implementacion DetalleAutorInsertar

--- app/src/main/java/com/grupo13/inventario/activities/DetalleAutorEliminarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DetalleAutorEliminarActivity.java
@@ -6,11 +6,16 @@ import android.os.Bundle;
 
 import com.grupo13.inventario.R;
 
+import butterknife.ButterKnife;
+
 public class DetalleAutorEliminarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_autor_eliminar);
+
+        //USO DE BUTTERKNIFE, SI NO PONEN ESTA LINEA NO SIRVE
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/grupo13/inventario/activities/DetalleAutorInsertarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DetalleAutorInsertarActivity.java
@@ -3,14 +3,53 @@ package com.grupo13.inventario.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 //Importante!!!
+import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
+import com.grupo13.inventario.modelo.DetalleAutor;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetalleAutorInsertarActivity extends AppCompatActivity {
+    ControlBD helper;
+    //Usando butterknife no es necesario hacer findViewById
+    @BindView(R.id.edtEscritoID)
+    EditText edtEscritoID;
+    @BindView(R.id.edtAutorID)
+    EditText edtAutorID;
+    @BindView(R.id.edtEsPrincipal)
+    CheckBox edtEspPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_autor_insertar);
+        helper = ControlBD.getInstance(this);
+
+        //IMPORTANTE SI NO NO SE PUEDE USAR BUTTERKNIFE!!!
+        ButterKnife.bind(this);
+    }
+
+    public void insertarDetalle(View v){
+        DetalleAutor nuevo = new DetalleAutor();
+        nuevo.escrito_id = Integer.parseInt(edtEscritoID.getText().toString());
+        nuevo.idAutor = Integer.parseInt(edtAutorID.getText().toString());
+        nuevo.esPrincipal = edtEspPrincipal.isChecked();
+
+        long idDetalle = helper.detalleAutorDao().insertarDetalleAutor(nuevo);
+
+        String mensaje = "";
+        if(idDetalle == 0 || idDetalle == -1){
+            mensaje = "El DetalleAutor no se pudo registrar con exito";
+        }
+        else{
+            mensaje = String.format("DetalleAutor registrado con ID %d",idDetalle);
+        }
+        Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
     }
 }
