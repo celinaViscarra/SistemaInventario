5983d8c6c0c6d95b1b418deeca56432941476dda
Marvin Alvarenga <marvinalexz@gmail.com>
2020-06-06 16:38:02 -0600

    Funcionalidad para agregar un nuevo autor y los strings correspondientes

---
+++ app/src/main/java/com/grupo13/inventario/activities/AutorInsertarActivity.java
@@ -0,0 +1,56 @@
+package com.grupo13.inventario.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteConstraintException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.grupo13.inventario.ControlBD;
+import com.grupo13.inventario.R;
+import com.grupo13.inventario.modelo.Autor;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AutorInsertarActivity extends AppCompatActivity {
+    ControlBD helper;
+
+    @BindView(R.id.txt_autor_nombre)
+    EditText txtNombre;
+    @BindView(R.id.txt_autor_apellido)
+    EditText txtApellido;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autor_insertar);
+        helper = ControlBD.getInstance(this);
+
+        ButterKnife.bind(this);
+    }
+
+    public void insertarAutorAutor(View v){
+        String mensaje = "";
+        try {
+            Autor nuevoAutor = new Autor();
+            nuevoAutor.nomAutor = txtNombre.getText().toString();
+            nuevoAutor.apeAutor = txtApellido.getText().toString();
+
+            long idAutor = helper.autorDao().insertarAutor(nuevoAutor);
+            if(idAutor == 0 || idAutor == -1){
+                mensaje = "Error al tratar de ingresar el registro a la Base de Datos.";
+            }
+            else{
+                mensaje = String.format("Autor registrado con ID %d", idAutor);
+            }
+        }catch (SQLiteConstraintException e){
+            mensaje = "Error al tratar de ingresar el registro a la Base de Datos.";
+        }
+        finally{
+            Toast.makeText(this,mensaje, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
