86b68ba890fd6ea645f62e63892fc66bc3c38896
Celina <xelaju_celina@yahoo.com>
2020-06-06 16:45:06 -0600

    Modificacion metodos insertar.

--- app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoInsertarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoInsertarActivity.java
@@ -3,14 +3,66 @@ package com.grupo13.inventario.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
+import com.grupo13.inventario.modelo.CatalogoEquipo;
 
 public class CatalogoEquipoInsertarActivity extends AppCompatActivity {
+    ControlBD helper;
+    EditText idCatalogo, idMarca, modeloEquipo, memoria, cantEquipo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo_equipo_insertar);
+        idCatalogo = (EditText) findViewById(R.id.editIdCatalogo);
+        idMarca = (EditText) findViewById(R.id.editIdMarca);
+        modeloEquipo = (EditText) findViewById(R.id.editModeloEquipo);
+        memoria = (EditText) findViewById(R.id.editMemoria);
+        cantEquipo = (EditText) findViewById(R.id.editCantEquipo);
+
+        helper = ControlBD.getInstance(this);
+    }
+
+    public void insertarCatalogoEquipo(View v){
+        String mensaje = "";
+        try {
+            String idCata = idCatalogo.getText().toString();
+            String idMark = idMarca.getText().toString();
+            String modelo = modeloEquipo.getText().toString();
+            int memo = Integer.parseInt(memoria.getText().toString());
+            int cantidad = Integer.parseInt(cantEquipo.getText().toString());
+
+            CatalogoEquipo nuevo = new CatalogoEquipo();
+            nuevo.idCatalogo = idCata;
+            nuevo.idMarca = idMark;
+            nuevo.modeloEquipo = modelo;
+            nuevo.memoria = memo;
+            nuevo.cantEquipo = cantidad;
+
+
+            long catalogo = helper.catalogoEquipoDao().insertarCatalogoEquipo(nuevo);
+            if(catalogo == 0 || catalogo == -1){
+                mensaje = "ERROR AL INSERTAR CATALÓGO EQUIPO";
+            }else{
+                mensaje = String.format("Registro insertado en la posicion %d", catalogo);
+            }
+        } catch (Exception e) {
+            mensaje = "ERROR AL INSERTAR CATALÓGO EQUIPO";
+        }
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v){
+        idMarca.setText("");
+        idCatalogo.setText("");
+        cantEquipo.setText("");
+        modeloEquipo.setText("");
+        memoria.setText("");
     }
 }
--- app/src/main/java/com/grupo13/inventario/activities/DetalleDescargosInsertarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DetalleDescargosInsertarActivity.java
@@ -3,10 +3,12 @@ package com.grupo13.inventario.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
@@ -29,12 +31,30 @@ public class DetalleDescargosInsertarActivity extends AppCompatActivity {
     }
 
     public void insertarDetalleDescargos(View v){
-        int idEquipo = Integer.parseInt(editIdEquipo.getText().toString());
-        int idDescargos = Integer.parseInt(editIdDescargos.getText().toString());
-        DetalleDescargos nuevo = new DetalleDescargos();
-        nuevo.idDescargo = idDescargos;
-        nuevo.idEquipo = idEquipo;
-        helper.detalleDescargosDao().insertarDetalleDescargos(nuevo);
+        String mensaje = "";
+        try {
+            int idEquipo = Integer.parseInt(editIdEquipo.getText().toString());
+            int idDescargos = Integer.parseInt(editIdDescargos.getText().toString());
+            DetalleDescargos nuevo = new DetalleDescargos();
+            nuevo.idDescargo = idDescargos;
+            nuevo.idEquipo = idEquipo;
+            long idDetalle = helper.detalleDescargosDao().insertarDetalleDescargos(nuevo);
+
+            if(idDetalle == 0 || idDetalle == -1){
+                mensaje = "Error al tratar de ingresar el registro a la Base de Datos.";
+            }
+            else{
+                mensaje = String.format("DetalleDescargos registrado con ID %d",idDetalle);
+            }
+        }catch (SQLiteConstraintException e){
+            mensaje = "Error al tratar de ingresar el registro a la Base de Datos.";
+        }
+        catch (NumberFormatException e){
+            mensaje = "Error en la entrada de datos, revisa por favor los datos ingresados.";
+        }
+        finally{
+            Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void limpiarTexto(View v){
