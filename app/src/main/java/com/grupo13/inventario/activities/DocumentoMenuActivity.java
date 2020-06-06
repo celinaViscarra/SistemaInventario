dc79405c354e60445fbb7bed639cfe83e7dddaa0
aquinojosue <aquinojosue3@gmail.com>
2020-06-06 15:20:25 -0600

    Fix typo

--- app/src/main/java/com/grupo13/inventario/activities/DocumentoMenuActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DocumentoMenuActivity.java
@@ -2,15 +2,45 @@ package com.grupo13.inventario.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.grupo13.inventario.R;
 
-public class DocumentoMenuActivity extends AppCompatActivity {
+public class DocumentoMenuActivity extends ListActivity {
+    String[] menu = {
+            "Insertar Documento",
+            "Consultar Documento",
+            "Actualizar Documento",
+            "Eliminar Documento"
+    };
+    String[] activities = {
+            "DocumentoInsertarActivity",
+            "DocumentoConsultarActivity",
+            "DocumentoActualizarActivity",
+            "DocumentoEliminarActivity"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_documento_menu);
+        //setContentView(R.layout.activity_documento_menu);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        try {
+            Class<?> clase = Class.forName("com.grupo13.inventario.activities."+activities[position]);
+            Intent inte = new Intent(this,clase);
+            startActivity(inte);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
