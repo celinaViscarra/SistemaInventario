31803de1e6d1acb0b61f9565d80c8c5f00f371b7
aquinojosue <aquinojosue3@gmail.com>
2020-06-06 14:07:37 -0600

    Creacion Activities Documento

---
+++ app/src/main/java/com/grupo13/inventario/activities/DocumentoInsertarActivity.java
@@ -0,0 +1,14 @@
+package com.grupo13.inventario.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocumentoInsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_documento_insertar);
+    }
+}
