e65d4af9c9d78f408450a3e2936a870f6159d219
Celina <xelaju_celina@yahoo.com>
2020-06-05 20:09:24 -0600

    Creación de los activities de Catalogo Equipo.

---
+++ app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoInsertarActivity.java
@@ -0,0 +1,16 @@
+package com.grupo13.inventario.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grupo13.inventario.R;
+
+public class CatalogoEquipoInsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogo_equipo_insertar);
+    }
+}
