e551d59d6ace01c944b3c00d22c34c1b64b7bc34
Celina <xelaju_celina@yahoo.com>
2020-06-07 16:13:16 -0600

    Modificacion CatalogoEquipoActualizar

--- app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoActualizarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoActualizarActivity.java
@@ -6,21 +6,26 @@ import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
 import com.grupo13.inventario.modelo.CatalogoEquipo;
 
+import java.util.List;
+
 public class CatalogoEquipoActualizarActivity extends AppCompatActivity {
     ControlBD helper;
-    EditText idCatalogo, idMarca, modelo, memoria, cantidad;
+    EditText idMarca, modelo, memoria, cantidad;
+    Spinner idCatalogo;
+    List<CatalogoEquipo> catalogos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo_equipo_actualizar);
-        idCatalogo = (EditText) findViewById(R.id.editIdCatalogo);
+        idCatalogo = (Spinner) findViewById(R.id.editIdCatalogo);
         idMarca = (EditText) findViewById(R.id.editIdMarca);
         modelo = (EditText) findViewById(R.id.editModeloEquipo);
         memoria = (EditText) findViewById(R.id.editMemoria);
@@ -32,20 +37,24 @@ public class CatalogoEquipoActualizarActivity extends AppCompatActivity {
     public void actualizarCatalogoEquipo(View v){
         String mensaje = "";
         try{
+            int posCatalogo = idCatalogo.getSelectedItemPosition();
             CatalogoEquipo catalogo = new CatalogoEquipo();
-            catalogo.idCatalogo = idCatalogo.getText().toString();
-            catalogo.idMarca = idMarca.getText().toString();
-            catalogo.modeloEquipo = modelo.getText().toString();
-            catalogo.memoria = Integer.parseInt(memoria.getText().toString());
-            catalogo.cantEquipo = Integer.parseInt(cantidad.getText().toString());
+            if(posCatalogo>0) {
+                catalogo.idCatalogo = catalogos.get(posCatalogo - 1).idCatalogo;
+                catalogo.idMarca = idMarca.getText().toString();
+                catalogo.modeloEquipo = modelo.getText().toString();
+                catalogo.memoria = Integer.parseInt(memoria.getText().toString());
+                catalogo.cantEquipo = Integer.parseInt(cantidad.getText().toString());
 
-            int filasAfectadas = helper.catalogoEquipoDao().actualizarCatalogoEquipo(catalogo);
-            if(filasAfectadas <= 0){
-                mensaje = "Error al tratar de actualizar el registro.";
-            }
-            else{
-                mensaje = String.format("Filas afectadas: %d",filasAfectadas);
-            }
+                int filasAfectadas = helper.catalogoEquipoDao().actualizarCatalogoEquipo(catalogo);
+                if (filasAfectadas <= 0) {
+                    mensaje = "Error al tratar de actualizar el registro.";
+                } else {
+                    mensaje = String.format("Filas afectadas: %d", filasAfectadas);
+                }
+            }else{
+                mensaje = "Por favor, seleccione una opcion valida.";
+                }
         }catch(NumberFormatException e){
             mensaje = "Error en la entrada de datos, revisa por favor los datos ingresados.";
         }catch (SQLiteConstraintException e){
@@ -57,7 +66,6 @@ public class CatalogoEquipoActualizarActivity extends AppCompatActivity {
     }
 
     public void limpiarTexto(View v){
-        idCatalogo.setText("");
         idMarca.setText("");
         modelo.setText("");
         memoria.setText("");
