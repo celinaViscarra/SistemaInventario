d6d3456f0805267b45b00cf7f70765b37a9dc5fd
Celina <xelaju_celina@yahoo.com>
2020-06-07 18:27:20 -0600

    Modificacion CatalogoEquipo

--- app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoConsultarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoConsultarActivity.java
@@ -4,43 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.grupo13.inventario.ControlBD;
 import com.grupo13.inventario.R;
 import com.grupo13.inventario.modelo.CatalogoEquipo;
+import com.grupo13.inventario.modelo.Marca;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CatalogoEquipoConsultarActivity extends AppCompatActivity {
     ControlBD helper;
-    EditText idCatalogo, idMarca, modelo, memoria, cantidad;
+    EditText  idMarca, modelo, memoria, cantidad;
+    Spinner idCatalogo;
+
+    List<CatalogoEquipo> catalogos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo_equipo_consultar);
-        idCatalogo = (EditText) findViewById(R.id.editIdCatalogo);
+        idCatalogo = (Spinner) findViewById(R.id.editIdCatalogo);
         idMarca = (EditText) findViewById(R.id.editIdMarca);
         modelo = (EditText) findViewById(R.id.editModeloEquipo);
         memoria = (EditText) findViewById(R.id.editMemoria);
         cantidad = (EditText) findViewById(R.id.editCantEquipo);
 
         helper = ControlBD.getInstance(this);
+        llenarSpiners();
     }
 
     public void consultarCatalogoEquipo(View v){
         String mensaje = "";
         try{
-            String idCatalogoEquipo = idCatalogo.getText().toString();
-            CatalogoEquipo catalogo = helper.catalogoEquipoDao().consultarCatalogoEquipo(idCatalogoEquipo);
-            if(catalogo == null){
-                mensaje = "No se encontraron datos";
+            int posCatalogo = idCatalogo.getSelectedItemPosition();
+            if(posCatalogo>0) {
+                String idCatalogo = catalogos.get(posCatalogo - 1).idCatalogo;
+                CatalogoEquipo catalogo = helper.catalogoEquipoDao().consultarCatalogoEquipo(idCatalogo);
+                if(catalogo == null){
+                    mensaje = "No se encontraron datos";
+                }else{
+                    mensaje = "Se encontro el registro, mostrando datos...";
+                    idMarca.setText(catalogo.idMarca);
+                    modelo.setText(catalogo.modeloEquipo);
+                    memoria.setText(catalogo.memoria);
+                    cantidad.setText(catalogo.cantEquipo);
+                }
             }else{
-                mensaje = "Se encontro el registro, mostrando datos...";
-                idMarca.setText(catalogo.idMarca);
-                modelo.setText(catalogo.modeloEquipo);
-                memoria.setText(catalogo.memoria);
-                cantidad.setText(catalogo.cantEquipo);
+                mensaje = "Por favor, seleccione una opcion valida.";
             }
         }catch(NumberFormatException e){
             mensaje = "Error en la entrada de datos, revisa por favor los datos ingresados.";
@@ -50,10 +65,24 @@ public class CatalogoEquipoConsultarActivity extends AppCompatActivity {
     }
 
     public void limpiarTexto(View v){
-        idCatalogo.setText("");
-        idMarca.setText("");
         modelo.setText("");
         memoria.setText("");
         cantidad.setText("");
+    }
+
+    public void llenarSpiners() {
+        catalogos = helper.catalogoEquipoDao().obtenerCatalogoEquipo();
+
+        ArrayList<String> idCatalogos = new ArrayList<>();
+
+        idCatalogos.add("** Seleccione un Catalogo **");
+
+        for (CatalogoEquipo catalogoEquipo : catalogos) {
+            idCatalogos.add(catalogoEquipo.idCatalogo);
+        }
+
+        ArrayAdapter<String> catalogoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, idCatalogos);
+
+        idCatalogo.setAdapter(catalogoAdapter);
     }
 }
