598dbcc9b68d6806cc5f0379e6fd0b7190c6af91
aquinojosue <aquinojosue3@gmail.com>
2020-06-07 23:50:00 -0600

    Agregar un descargo al llenar base y cambio del calendario.

--- app/src/main/java/com/grupo13/inventario/activities/DescargosConsultarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DescargosConsultarActivity.java
@@ -15,6 +15,7 @@ import com.grupo13.inventario.modelo.Descargos;
 import com.grupo13.inventario.modelo.Docente;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class DescargosConsultarActivity extends AppCompatActivity {
@@ -50,13 +51,15 @@ public class DescargosConsultarActivity extends AppCompatActivity {
                     mensaje = "No se encontraron datos";
                 } else {
                     mensaje = "Se encontro el registro, mostrando datos...";
-                    idOrigen.setText(descargos.ubicacion_origen_id);
-                    idDestino.setText(descargos.ubicacion_destino_id);
+                    idOrigen.setText(descargos.ubicacion_origen_id+"");
+                    idDestino.setText(descargos.ubicacion_destino_id+"");
                     int dia, mes, anio;
-                    dia = descargos.fechaDescargos.getDay();
-                    mes = descargos.fechaDescargos.getMonth();
-                    anio = descargos.fechaDescargos.getYear();
-                    descargoFecha.setText(String.format("%d—%d—%d", dia, mes, anio));
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(descargos.fechaDescargos);
+                    dia = cal.get(Calendar.DAY_OF_MONTH);
+                    mes = cal.get(Calendar.MONTH) + 1;
+                    anio = cal.get(Calendar.YEAR);
+                    descargoFecha.setText(String.format("%d-%d-%d",dia,mes,anio));
                 }
             }else {
                 mensaje = "Por favor seleccione los campos para poder consultar.";
