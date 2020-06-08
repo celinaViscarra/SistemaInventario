3dd484cd21a255658bdd7cee0f01ba9d5801ce9b
aquinojosue <aquinojosue3@gmail.com>
2020-06-07 23:03:11 -0600

    Modificaciones a actualizar Documento

--- app/src/main/java/com/grupo13/inventario/activities/DocumentoActualizarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DocumentoActualizarActivity.java
@@ -24,7 +24,6 @@ import butterknife.ButterKnife;
 
 public class DocumentoActualizarActivity extends AppCompatActivity {
     @BindViews({
-            R.id.edtEscritoID,
             R.id.edtISBN,
             R.id.edtEdicion,
             R.id.edtEditorial,
@@ -34,12 +33,14 @@ public class DocumentoActualizarActivity extends AppCompatActivity {
 
     @BindViews({
             R.id.edtTipoProductoID,
-            R.id.edtIdiomaID
+            R.id.edtIdiomaID,
+            R.id.edtEscritoID
     })
     List<Spinner> spinners;
 
     List<Idiomas> idiomas;
     List<TipoProducto> tiposProducto;
+    List<Documento> documentos;
 
     ControlBD helper;
 
@@ -56,19 +57,29 @@ public class DocumentoActualizarActivity extends AppCompatActivity {
     public void llenarSpinners(){
         idiomas = helper.idiomasDao().obtenerIdiomas();
         tiposProducto = helper.tipoProductoDao().obtenerTipos();
+        documentos = helper.documentoDao().obtenerDocumentos();
 
         ArrayList<String> nomIdiomas = new ArrayList<>();
         ArrayList<String> nomTiposProducto = new ArrayList<>();
+        ArrayList<String> nomDocumentos = new ArrayList<>();
+        nomIdiomas.add("** Selecciona un idioma **");
+        nomTiposProducto.add("** Selecciona un Tipo de Producto **");
+        nomDocumentos.add("** Selecciona un Documento **");
         for(Idiomas idioma: idiomas){
             nomIdiomas.add(idioma.nombreIdioma);
         }
         for(TipoProducto tipoProducto: tiposProducto){
             nomTiposProducto.add(tipoProducto.nomTipoProducto);
         }
+        for(Documento pivote: documentos) nomDocumentos.add(pivote.titulo);
+
         ArrayAdapter<String> adapterIdiomas = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomIdiomas);
         ArrayAdapter<String> adapterTiposProducto = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomTiposProducto);
+        ArrayAdapter<String> adapterDocumentos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomDocumentos);
+
         spinners.get(0).setAdapter(adapterTiposProducto);
         spinners.get(1).setAdapter(adapterIdiomas);
+        spinners.get(2).setAdapter(adapterDocumentos);
     }
     public void limpiarTexto(View v){
         for(Spinner spinner: spinners)
@@ -80,21 +91,35 @@ public class DocumentoActualizarActivity extends AppCompatActivity {
     public void actualizarDocumento(View v){
         String mensaje = "";
         try{
-            Documento doc = new Documento();
-            doc.idEscrito = Integer.parseInt(editTextList.get(0).getText().toString());
-            doc.tipo_producto_id = tiposProducto.get(spinners.get(0).getSelectedItemPosition()).idTipoProducto;
-            doc.idioma_id = idiomas.get(spinners.get(1).getSelectedItemPosition()).idIdioma;
-            doc.isbn = editTextList.get(1).getText().toString();
-            doc.edicion = editTextList.get(2).getText().toString();
-            doc.editorial = editTextList.get(3).getText().toString();
-            doc.titulo = editTextList.get(4).getText().toString();
+            int posDocumento = spinners.get(2).getSelectedItemPosition();
+            int posIdioma = spinners.get(1).getSelectedItemPosition();
+            int posTipoproducto = spinners.get(0).getSelectedItemPosition();
+            boolean textosVacios = false;
+            for(EditText pivote: editTextList){
+                textosVacios = pivote.getText().toString().equals("") || textosVacios;
+            }
+            if(posDocumento > 0 && posIdioma >0 && posTipoproducto > 0 && !textosVacios){
+                Documento doc = new Documento();
+                doc.idEscrito = documentos.get(posDocumento - 1).idEscrito;
+                doc.tipo_producto_id = tiposProducto.get(posTipoproducto - 1).idTipoProducto;
+                doc.idioma_id = idiomas.get(posIdioma - 1).idIdioma;
+                doc.isbn = editTextList.get(0).getText().toString();
+                doc.edicion = editTextList.get(1).getText().toString();
+                doc.editorial = editTextList.get(2).getText().toString();
+                doc.titulo = editTextList.get(3).getText().toString();
 
-            int filasAfectadas = helper.documentoDao().actualizarDocumento(doc);
-            if(filasAfectadas <= 0){
-                mensaje = "Error al tratar de actualizar el registro.";
+                int filasAfectadas = helper.documentoDao().actualizarDocumento(doc);
+                if(filasAfectadas <= 0){
+                    mensaje = "Error al tratar de actualizar el registro.";
+                }
+                else{
+                    mensaje = String.format("Filas afectadas: %d",filasAfectadas);
+                }
+            } else if(textosVacios){
+                mensaje = "Revisar que ningun campo de texto vaya vacío.";
             }
             else{
-                mensaje = String.format("Filas afectadas: %d",filasAfectadas);
+                mensaje = "Seleccione una opcion por favor.";
             }
 
         } catch (SQLiteConstraintException e){
