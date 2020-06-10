34f40cb5d2ea2000d83de727059a8d809d0a6d13
Josué Aquino <aquinojosue3@gmail.com>
2020-06-10 01:05:05 -0600

    Consultar Documento, implementacion en sql y webservice

--- app/src/main/java/com/grupo13/inventario/activities/DocumentoConsultarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DocumentoConsultarActivity.java
@@ -2,29 +2,50 @@ package com.grupo13.inventario.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteConstraintException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.grupo13.inventario.ControlBD;
+import com.grupo13.inventario.ControlWS;
 import com.grupo13.inventario.R;
 import com.grupo13.inventario.modelo.Documento;
 import com.grupo13.inventario.modelo.Idiomas;
 import com.grupo13.inventario.modelo.TipoProducto;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 public class DocumentoConsultarActivity extends AppCompatActivity {
+    //Modo datos 1 = SQLITE
+    //2 = WebService
+    int modo_datos = 1;
+    //URL de nuestra peticion.
+    private final String urlIdioma = "http://grupo13pdm.ml/inventariows/obtenerlista.php?tabla=idiomas";
+    private final String urlTipoProducto = "http://grupo13pdm.ml/inventariows/obtenerlista.php?tabla=tipoproducto";
+    private final String urlDocumento = "http://grupo13pdm.ml/inventariows/documento/obtenerlista.php";
+    private final String urlConsulta = "http://grupo13pdm.ml/inventariows/documento/consultar.php";
+    @BindView(R.id.btnModo)
+    Button btnModo;
     @BindViews({
             R.id.edtISBN,
             R.id.edtEdicion,
@@ -52,41 +73,28 @@ public class DocumentoConsultarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_documento_consultar);
         ButterKnife.bind(this);
         helper = ControlBD.getInstance(this);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         llenarSpinners();
     }
-
+    public void cambiarModo(View v){
+        if (modo_datos == 1) modo_datos = 2;
+        else modo_datos = 1;
+        //Si modo datos esta en 1, el texto sera cambias a WS, caso contrario en modo datos 2
+        btnModo.setText((modo_datos == 1)? R.string.cambiar_a_ws: R.string.cambiar_a_sqlite);
+        //Ejecutamos el metodo para llenar la lista.
+        llenarSpinners();
+        limpiarTexto(v);
+    }
     public void limpiarTexto(View v){
         for(Spinner pivote: spinners) pivote.setSelection(0);
         for(EditText pivote: editTextList) pivote.setText("");
     }
 
     public void llenarSpinners(){
-        documentos = helper.documentoDao().obtenerDocumentos();
-        idiomas = helper.idiomasDao().obtenerIdiomas();
-        tiposProducto = helper.tipoProductoDao().obtenerTipos();
-
-        ArrayList<String> nomIdiomas = new ArrayList<>();
-        nomIdiomas.add("Ningun elemento seleccionado");
-        ArrayList<String> nomTiposProducto = new ArrayList<>();
-        nomTiposProducto.add("Ningun elemento seleccionado");
-        ArrayList<String> nomDocumentos = new ArrayList<>();
-        nomDocumentos.add("Ningun elemento seleccionado");
-
-        for(Documento documento: documentos) nomDocumentos.add(documento.titulo);
-        for(Idiomas idioma: idiomas) nomIdiomas.add(idioma.nombreIdioma);
-        for(TipoProducto tipoProducto: tiposProducto) nomTiposProducto.add(tipoProducto.nomTipoProducto);
-
-        ArrayAdapter<String> adapterIdiomas = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomIdiomas);
-        ArrayAdapter<String> adapterTiposProducto = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomTiposProducto);
-        ArrayAdapter<String> adapterDocumentos = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nomDocumentos);
-
-        spinners.get(0).setEnabled(false);
-        spinners.get(0).setClickable(false);
-        spinners.get(1).setEnabled(false);
-        spinners.get(1).setClickable(false);
-        spinners.get(0).setAdapter(adapterTiposProducto);
-        spinners.get(1).setAdapter(adapterIdiomas);
-        spinners.get(2).setAdapter(adapterDocumentos);
+        new LlenarLista(this).execute();
     }
 
     public void consultarDocumento(View v){
@@ -95,7 +103,30 @@ public class DocumentoConsultarActivity extends AppCompatActivity {
             int posSeleccionada = spinners.get(2).getSelectedItemPosition();
             if(posSeleccionada > 0){
                 int idEscrito = documentos.get(posSeleccionada - 1).idEscrito;
-                Documento docConsultado = helper.documentoDao().consultarDocumento(idEscrito);
+                Documento docConsultado = null;
+
+                if(modo_datos == 1){
+                    docConsultado = helper.documentoDao().consultarDocumento(idEscrito);
+                }else{
+                    JSONObject elementoConsulta = new JSONObject();
+                    elementoConsulta.put("escrito_id",Integer.toString(idEscrito));
+
+                    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                    params.add(new BasicNameValuePair("elementoConsulta",elementoConsulta.toString()));
+                    String respuesta = ControlWS.post(urlConsulta,params, this);
+                    JSONObject documentoConsulta = new JSONArray(respuesta).getJSONObject(0);
+                    if(documentoConsulta.length() != 0){
+                        docConsultado = new Documento();
+                        docConsultado.idEscrito = documentoConsulta.getInt("ESCRITO_ID");
+                        docConsultado.tipo_producto_id = documentoConsulta.getInt("TIPO_PRODUCTO_ID");
+                        docConsultado.idioma_id = documentoConsulta.getInt("IDIOMA_ID");
+                        docConsultado.isbn = documentoConsulta.getString("ISBN");
+                        docConsultado.edicion = documentoConsulta.getString("EDICION");
+                        docConsultado.editorial = documentoConsulta.getString("EDITORIAL");
+                        docConsultado.titulo = documentoConsulta.getString("TITULO");
+                    }
+                }
+
                 if(docConsultado == null){
                     mensaje = "No se encontraron datos";
 
@@ -120,7 +151,6 @@ public class DocumentoConsultarActivity extends AppCompatActivity {
                     }
                     spinners.get(1).setSelection(pos+1);
                 }
-
             }
             else{
                 mensaje = "No se ha seleccionado ningun documento a consultar.";
@@ -131,9 +161,82 @@ public class DocumentoConsultarActivity extends AppCompatActivity {
         }
         catch (NumberFormatException e) {
             mensaje = "Error en la entrada de datos, revisa por favor los datos ingresados.";
-        }
-        finally {
+        } catch (JSONException e) {
+            mensaje = "Error de parseo JSON";
+        } finally {
             Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class LlenarLista extends AsyncTask<String, String, String> {
+        Context ctx;
+        public LlenarLista(Context ctx){
+            this.ctx = ctx;
+        }
+        ArrayList<String> nomIdiomas = new ArrayList<>();
+        ArrayList<String> nomTiposProducto = new ArrayList<>();
+        ArrayList<String> nomDocumentos = new ArrayList<>();
+        //Primer paso, antes de ejecutar nuestra consulta
+        @Override
+        protected void onPreExecute() {
+            //Desactivamos el boton para que no le den click hasta que termine
+            btnModo.setEnabled(false);
+        }
+        //Paso 2: la consulta que se hace en otro hilo.
+        @Override
+        protected String doInBackground(String... strings) {
+            //Empieza consulta, revisamos en que modo tenemos los datos.
+            switch (modo_datos){
+                //Caso 1: Modo SQLite
+                case 1:{
+                    idiomas = helper.idiomasDao().obtenerIdiomas();
+                    tiposProducto = helper.tipoProductoDao().obtenerTipos();
+                    documentos = helper.documentoDao().obtenerDocumentos();
+                    break;
+                }
+                //Caso 2: Modo WebService
+                case 2:{
+                    String jsonIdiomas = ControlWS.get(urlIdioma, ctx);
+                    String jsonTiposProducto = ControlWS.get(urlTipoProducto, ctx);
+                    String jsonDocumento = ControlWS.get(urlDocumento, ctx);
+
+                    idiomas = ControlWS.obtenerListaIdioma(jsonIdiomas, ctx);
+                    tiposProducto = ControlWS.obtenerListaTipoProducto(jsonTiposProducto, ctx);
+                    documentos = ControlWS.obtenerListaDocumento(jsonDocumento, ctx);
+                    break;
+                }
+            }
+
+            nomIdiomas.add("** Selecciona un idioma **");
+            nomTiposProducto.add("** Selecciona un Tipo de Producto **");
+            nomDocumentos.add("** Selecciona un Documento **");
+            for(Idiomas idioma: idiomas){
+                nomIdiomas.add(idioma.nombreIdioma);
+            }
+            for(TipoProducto tipoProducto: tiposProducto){
+                nomTiposProducto.add(tipoProducto.nomTipoProducto);
+            }
+
+            for(Documento pivote: documentos) nomDocumentos.add(pivote.titulo);
+            return null;
+        }
+        //paso 3: despues de la consulta
+        @Override
+        protected void onPostExecute(String result) {
+            ArrayAdapter<String> adapterIdiomas = new ArrayAdapter<>(ctx, android.R.layout.simple_spinner_dropdown_item, nomIdiomas);
+            ArrayAdapter<String> adapterTiposProducto = new ArrayAdapter<>(ctx, android.R.layout.simple_spinner_dropdown_item, nomTiposProducto);
+            ArrayAdapter<String> adapterDocumentos = new ArrayAdapter<>(ctx, android.R.layout.simple_spinner_dropdown_item, nomDocumentos);
+
+            spinners.get(0).setEnabled(false);
+            spinners.get(0).setClickable(false);
+            spinners.get(1).setEnabled(false);
+            spinners.get(1).setClickable(false);
+
+            spinners.get(0).setAdapter(adapterTiposProducto);
+            spinners.get(1).setAdapter(adapterIdiomas);
+            spinners.get(2).setAdapter(adapterDocumentos);
+
+            btnModo.setEnabled(true);
         }
     }
 }
