21f37f2ea2f7a0546f1613f66bef1eff4f2bb5bc
aquinojosue <aquinojosue3@gmail.com>
2020-06-10 18:25:00 -0600

    implementacion consultar detalleautor en ws

--- app/src/main/java/com/grupo13/inventario/activities/DetalleAutorConsultarActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/DetalleAutorConsultarActivity.java
@@ -2,20 +2,31 @@ package com.grupo13.inventario.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteConstraintException;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.grupo13.inventario.ControlBD;
+import com.grupo13.inventario.ControlWS;
 import com.grupo13.inventario.R;
 import com.grupo13.inventario.modelo.Autor;
 import com.grupo13.inventario.modelo.DetalleAutor;
 import com.grupo13.inventario.modelo.Documento;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +35,13 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetalleAutorConsultarActivity extends AppCompatActivity {
+    int modo_datos = 1;
+    private final String urlDocumentos = "http://grupo13pdm.ml/inventariows/documento/obtenerlista.php";
+    private final String urlAutores = "http://grupo13pdm.ml/inventariows/autor/obtenerlista.php";
+    private final String urlConsulta = "http://grupo13pdm.ml/inventariows/detalleautor/consultar.php";
+    @BindView(R.id.btnModo)
+    Button btnModo;
+
     ControlBD helper;
     //Usando butterknife no es necesario hacer findViewById
     @BindView(R.id.edtEscritoID)
@@ -44,19 +62,38 @@ public class DetalleAutorConsultarActivity extends AppCompatActivity {
         helper = ControlBD.getInstance(this);
         ButterKnife.bind(this);
         llenarSpinners();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
     public void consultarDetalleAutor(View v){
 
         String mensaje = "";
         try {
-            DetalleAutor consulta;
+            DetalleAutor consulta = null;
 
             int posDoc = edtEscritoID.getSelectedItemPosition();
             int posAutor = edtAutorID.getSelectedItemPosition();
             if(posAutor > 0 && posDoc > 0){
                 int idAutor = autores.get(posAutor - 1).idAutor;
                 int escrito_id = documentos.get(posDoc - 1).idEscrito;
-                consulta = helper.detalleAutorDao().consultarDetalle(idAutor, escrito_id);
+                if(modo_datos == 1){
+                    consulta = helper.detalleAutorDao().consultarDetalle(idAutor, escrito_id);
+                }else{
+                    JSONObject elementoConsulta = new JSONObject();
+                    elementoConsulta.put("escrito_id",escrito_id);
+                    elementoConsulta.put("idAutor",idAutor);
+                    List<NameValuePair> params = new ArrayList<>();
+                    params.add(new BasicNameValuePair("elementoConsulta",elementoConsulta.toString()));
+                    String respuesta = ControlWS.post(urlConsulta,params,this);
+                    JSONObject detalleConsulta = new JSONArray(respuesta).getJSONObject(0);
+                    if(detalleConsulta.length() != 0){
+                        consulta = new DetalleAutor();
+                        consulta.escrito_id = detalleConsulta.getInt("ESCRITO_ID");
+                        consulta.idAutor = detalleConsulta.getInt("IDAUTOR");
+                        consulta.esPrincipal = (detalleConsulta.getInt("ESPRINCIPAL")==1);
+                    }
+
+                }
                 if(consulta == null){
                     mensaje = "No se encontraron datos";
                 }
@@ -70,31 +107,79 @@ public class DetalleAutorConsultarActivity extends AppCompatActivity {
             else{
                 mensaje = "Por favor, seleccione una opcion valida.";
             }
-        }
-        finally{
+        } catch (SQLiteConstraintException | JSONException e) {
+            mensaje = "Error en la consulta";
+        } finally{
             Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
         }
     }
 
+    public void cambiarModo(View v){
+        if(modo_datos == 1) modo_datos = 2;
+        else modo_datos = 1;
+
+        btnModo.setText((modo_datos == 1) ? R.string.cambiar_a_ws: R.string.cambiar_a_sqlite);
+        llenarSpinners();
+        limpiar(v);
+    }
+
+    public void limpiar(View v){
+        edtEscritoID.setSelection(0);
+        edtAutorID.setSelection(0);
+        edtEsPrincipal.setChecked(false);
+    }
+
     public void llenarSpinners(){
-        documentos = helper.documentoDao().obtenerDocumentos();
-        autores = helper.autorDao().obtenerAutores();
+        new LlenarLista(this).execute();
+    }
+
+    public class LlenarLista extends AsyncTask<String, String, String> {
+        Context ctx;
 
         ArrayList<String> nombreDocumentos = new ArrayList<>();
         ArrayList<String> nombreAutores = new ArrayList<>();
-        nombreDocumentos.add("**Seleccione un documento**");
-        nombreAutores.add("**Seleccione un autor**");
-        for(Documento documento: documentos){
-            nombreDocumentos.add(documento.titulo);
-        }
-        for(Autor autor: autores){
-            nombreAutores.add(String.format("%s %s",autor.nomAutor,autor.apeAutor));
+
+        public LlenarLista(Context ctx){
+            this.ctx = ctx;
         }
 
-        ArrayAdapter<String> docAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nombreDocumentos);
-        ArrayAdapter<String> autorAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nombreAutores);
+        @Override
+        protected void onPreExecute(){
+            btnModo.setEnabled(false);
+        }
 
-        edtEscritoID.setAdapter(docAdapter);
-        edtAutorID.setAdapter(autorAdapter);
+        @Override
+        protected String doInBackground(String... strings){
+            switch (modo_datos){
+                case 1:{
+                    documentos = helper.documentoDao().obtenerDocumentos();
+                    autores = helper.autorDao().obtenerAutores();
+                    break;
+                }
+                case 2:{
+                    String jsonDocumentos = ControlWS.get(urlDocumentos, ctx);
+                    String jsonAutores = ControlWS.get(urlAutores, ctx);
+
+                    documentos = ControlWS.obtenerListaDocumento(jsonDocumentos, ctx);
+                    autores = ControlWS.obtenerListaAutor(jsonAutores, ctx);
+                    break;
+                }
+            }
+            nombreDocumentos.add("** Seleccione un documento, por favor **");
+            nombreAutores.add("** Seleccione un autor, por favor **");
+            for(Documento pivote: documentos) nombreDocumentos.add(pivote.titulo);
+            for(Autor pivote: autores) nombreAutores.add(String.format("%s %s", pivote.nomAutor, pivote.apeAutor));
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            ArrayAdapter<String> docAdapter = new ArrayAdapter<>(ctx, android.R.layout.simple_spinner_dropdown_item, nombreDocumentos);
+            ArrayAdapter<String> autorAdapter = new ArrayAdapter<>(ctx, android.R.layout.simple_spinner_dropdown_item, nombreAutores);
+
+            edtEscritoID.setAdapter(docAdapter);
+            edtAutorID.setAdapter(autorAdapter);
+            btnModo.setEnabled(true);
+        }
     }
 }
