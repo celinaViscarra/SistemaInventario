e07cd2c6307bd38b824571844d905d8967d30e56
Celina <xelaju_celina@yahoo.com>
2020-06-11 18:15:14 -0600

    Este va porque Josué actualizó. Ojalá te ahogues con el café.

---
+++ app/src/main/java/com/grupo13/inventario/activities/CatalogoEquipoActualizarWSActivity.java
@@ -0,0 +1,14 @@
+package com.grupo13.inventario.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.grupo13.inventario.R;
+
+public class CatalogoEquipoActualizarWSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalogo_equipo_actualizar_ws);
+    }
+}
