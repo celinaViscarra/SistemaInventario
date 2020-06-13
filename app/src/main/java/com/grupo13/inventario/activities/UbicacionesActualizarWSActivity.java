5c47f8abf23cba87f5eb250f88df5e44277b8e1c
bhmarindelao <brmarinag@gmail.com>
2020-06-12 20:33:29 -0600

    Añadido el WebService de EliminarUbicaciones y sus permisos (agregados también el consultar y eliminar WS base)

---
+++ app/src/main/java/com/grupo13/inventario/activities/UbicacionesActualizarWSActivity.java
@@ -0,0 +1,16 @@
+package com.grupo13.inventario.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grupo13.inventario.R;
+
+public class UbicacionesActualizarWSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubicaciones_actualizar_w_s);
+    }
+}
