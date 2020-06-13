bb091a5f6b5b0d4d2f887e8ad60367f458f084fc
bhmarindelao <brmarinag@gmail.com>
2020-06-12 21:22:52 -0600

    Actualizar Ubicaciones WS y sus respectivos permisos

--- app/src/main/java/com/grupo13/inventario/ControlBD.java
+++ app/src/main/java/com/grupo13/inventario/ControlBD.java
@@ -452,13 +452,15 @@ public abstract class ControlBD extends RoomDatabase {
         OpcionCrud o77 = new OpcionCrud("077","UbicacionesInsertarWS",19);
         OpcionCrud o78 = new OpcionCrud("078","UbicacionesEliminarWS",19);
         OpcionCrud o79 = new OpcionCrud("079","UbicacionesConsultarWS",19);
+        OpcionCrud o80 = new OpcionCrud("080","UbicacionesActualizarWS",19);
 
         // Agregar aqui las TODAS las opciones disponibles
         OpcionCrud opciones[] = {
                 o1, o2, o3, o4, o5, o6, o7, o8, o9, o10, o11, o12, o13, o14, o15, o16, o17, o18, o19, o20,
                 o21, o22, o23, o24, o25, o26, o27, o28, o29, o30, o31, o32, o33, o34, o35, o36, o37, o38, o39, o40,
                 o41, o42, o43, o44, o45, o46, o47, o48, o49, o50, o51, o52, o53, o54, o55, o56, o57, o58, o59, o60,
-                o61, o62, o63, o64, o65, o66, o67, o68, o69, o70, o71, o72, o73, o74, o75, o76, o77, o78, o79
+                o61, o62, o63, o64, o65, o66, o67, o68, o69, o70, o71, o72, o73, o74, o75, o76, o77, o78, o79, o80,
+
             };
 
         // Guarda todas las opciones disponibles
--- app/src/main/java/com/grupo13/inventario/activities/UbicacionesActualizarWSActivity.java
+++ app/src/main/java/com/grupo13/inventario/activities/UbicacionesActualizarWSActivity.java
@@ -3,14 +3,75 @@ package com.grupo13.inventario.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.grupo13.inventario.ControlWS;
 import com.grupo13.inventario.R;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class UbicacionesActualizarWSActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtUbicacionID)
+    EditText edtUbicacionID;
+    @BindView(R.id.edtNombreUbicacion)
+    EditText edtNombreUbicacion;
+
+    private final String url = "https://eisi.fia.ues.edu.sv/eisi13/inventariows/ubicaciones/actualizar.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubicaciones_actualizar_w_s);
+        ButterKnife.bind(this);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    public void actualizarUbicacionesWS(View v){
+        String mensaje = "";
+        String ubicacion_id = edtUbicacionID.getText().toString();
+        String ubicacion_nombre = edtNombreUbicacion.toString();
+
+        if (!ubicacion_id.isEmpty() && !ubicacion_nombre.isEmpty()){
+            try {
+                JSONObject elementoActualizar = new JSONObject();
+                elementoActualizar.put("ubicacion_id",ubicacion_id);
+                elementoActualizar.put("ubicacion_nombre",ubicacion_nombre);
+
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("elementoActualizar",elementoActualizar.toString()));
+
+                String respuesta = ControlWS.post(url,params,this);
+                JSONObject resp = new JSONObject(respuesta);
+                int resultado = resp.getInt("resultado");
+                if (resultado == 1){
+                    mensaje = "Actualizado con exito.";
+                } else
+                    mensaje = "El dato no existe.";
+            } catch (JSONException e){
+                mensaje = "Error en el parseo";
+            } finally {
+                Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(this,"Complete todos los campos",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void limpiar(View v){
+        edtUbicacionID.setText("");
+        edtNombreUbicacion.setText("");
     }
 }
